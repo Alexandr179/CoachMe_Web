8bf95d4bd1e58573ea35c3d19ab7a6b0845079b6
Alexandr <Orange179@mail.ru>
2021-01-26 14:25:39 +0300

    init_start_to_host_revert_to_java_8_fix_ from_Ubuntu

--- src/main/java/ru/coach/web/security/details/UserDetailsServiceImpl.java
+++ src/main/java/ru/coach/web/security/details/UserDetailsServiceImpl.java
@@ -14,8 +14,11 @@ public class UserDetailsServiceImpl implements UserDetailsService {
     private UserRepository userRepository;
 
     @Override
-    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
-        User user = userRepository.findFirstByEmail(email).orElseThrow(() -> {throw new UsernameNotFoundException("User not found");});
+    public UserDetails loadUserByUsername(String email) {// throws UsernameNotFoundException
+//        User user = userRepository.findFirstByEmail(email).orElseThrow(() -> {throw new UsernameNotFoundException("User not found");});
+
+        //  unreported exception java.lang.Throwable... на сервере-Ubuntu выдает
+        User user = userRepository.findFirstByEmail(email).orElse(null);
         return new UserDetailsImpl(user);
     }
 }
